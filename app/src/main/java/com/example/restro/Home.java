67f2ddfb8124f32e6410eada95be3c4cc367c55c
Ashutosh Gautam <ashutoshgautam9628@outlook.com>
2020-10-25 09:04:47 -0700

    Update Home.java

--- app/src/main/java/com/example/restro/Home.java
+++ app/src/main/java/com/example/restro/Home.java
@@ -1,27 +1,65 @@
 package com.example.restro;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.net.Uri;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
    Button btnLogout;
    private TextView yourTextView;
    FirebaseAuth mFirebaseAuth;
    private FirebaseAuth.AuthStateListener mAuthStateListner;
+   DrawerLayout drawerLayout;
+   NavigationView navigationView;
+   Toolbar toolbar;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        /*-----------------hooks-------------*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        /*-----------------toolbar-------------*/
+
+        setSupportActionBar(toolbar);
+
+        Menu menu=navigationView.getMenu();
+        menu.findItem(R.id.nav_logout).setVisible(false);
+        menu.findItem(R.id.nav_profile).setVisible(false);
+        /*-----------------authecinte-------------*/
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        /*-----------------authecinte-------------*/
+
         btnLogout = findViewById(R.id.logout);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
@@ -48,4 +86,39 @@ public class Home extends AppCompatActivity {
     {
          Intent intent=new Intent(Home.this,Restaurant.class);
         startActivity(intent);
-    }}
+    }
+
+    /*--------------------------------------------------------*/
+
+
+
+    public void onBackPressed()
+    {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId())
+        {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_login:
+                Intent intent=new Intent(Home.this,Login_activity.class);
+                startActivity(intent);
+                break;
+
+                case R.id.nav_share:
+                Toast.makeText(this,"share",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
